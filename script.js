3d18961d42de7f9f9d072d0baea77fc6ce8a1609
Raj Joshi <rajjoshipawan@gmail.com>
2024-09-23 22:03:21 +0100

    Initial commit of Raj Marketing website

---
+++ script.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const counters = document.querySelectorAll('.stat-number');
+  const speed = 200;
+
+  counters.forEach(counter => {
+    const updateCount = () => {
+      const target = +counter.getAttribute('data-target');
+      const count = +counter.innerText;
+      const increment = target / speed;
+
+      if (count < target) {
+        counter.innerText = Math.ceil(count + increment);
+        setTimeout(updateCount, 1);
+      } else {
+        counter.innerText = target;
+      }
+    };
+
+    updateCount();
+  });
+});
